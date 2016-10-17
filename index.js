37b94291c78e6ed1595f8ba01a8076b01afb8be8
Greg Allen <greg@firstandthird.com>
2016-10-17 10:54:28 -0700

    wip

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+const wreck = require('wreck');
+const crypto = require('crypto');
+class MailchimpSubscribe {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseUrl = `https://${apiKey.split('-')[1]}.api.mailchimp.com/3.0`;
+  }
+
+  updateUser(listId, email, interests, mergeVars, status, done) {
+    const emailHash = crypto.createHash('md5').update(email).digest('hex');
+
+    const data = {
+      interests,
+      status,
+      email_address: email,
+      status_if_new: status,
+      merge_fields: mergeVars
+    };
+
+    const endpoint = `${this.baseUrl}/lists/${listId}/members/${emailHash}`;
+    wreck.put(url, {
+      json: true,
+      payload: JSON.stringify(data)
+    }, (err, response, json) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, json);
+    });
+  }
+
+  subscribe(listId, email, interests, mergeVars, done) {
+    this.updateUser(listId, email, interests, mergeVars, 'subscribed', done);
+  }
+}
+
+module.exports = MailchimpSubscribe;
+
---
+++ test/test.js
@@ -0,0 +1,12 @@
+const tape = require('tape');
+
+test('subscribe', function (t) {
+  t.plan(2);
+
+  t.equal(typeof Date.now, 'function');
+  var start = Date.now();
+
+  setTimeout(function () {
+    t.equal(Date.now() - start, 100);
+  }, 100);
+});
